8d1ca5fe0787d52d441a56871610ebf9d81244c8
erdenayates <71449664+erdenayates@users.noreply.github.com>
2021-06-27 21:55:55 +0300

    test example lane added
    test example lane added

--- app2/HelloWorld.java
+++ app2/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Example lane for Jenkins changeset entegration. This message written by Java");
+    System.out.println("Test example lane for Jenkins changeset entegration. This message written by Java");
   }
 }
