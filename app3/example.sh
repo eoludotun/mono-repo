984ad41b4b981d4962ff823991197a4f75eed7ab
eoludotun <eoludotun@users.noreply.github.com>
2021-10-16 20:04:33 -0400

    Update example.sh

--- app3/example.sh
+++ app3/example.sh
@@ -1,6 +1,3 @@
 
 echo Example lane for Jenkins changeset entegration. This message written by Bash
-echo Example lane for Jenkins changeset entegration. This message written by Bash
-echo Example lane for Jenkins changeset entegration. This message written by Bash
-echo Example lane for Jenkins changeset entegration. This message written by Bash
-echo Example lane for Jenkins changeset entegration. This message written by Bash
+
